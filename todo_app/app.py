984a7874c6bfc3b5d31ac3c60873b8e6ca8ea82d
Ted While <ted.while@softwire.com>
2024-01-24 16:48:23 +0000

    Implement completing items and improve styling

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect
 
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_items, add_item
+from todo_app.data.session_items import get_items, add_item, get_item, save_item 
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -18,4 +18,11 @@ def create():
     add_item(title)
 
     return redirect('/')
+
+@app.route('/<int:id>/complete/', methods=['POST'])
+def complete(id):
+    item = get_item(id)
+    item['status'] = "Completed"
+    save_item(item)
     
+    return redirect('/') 
